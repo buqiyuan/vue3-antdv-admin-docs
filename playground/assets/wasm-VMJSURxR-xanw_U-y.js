d3e42c70c4c8bea9fc109df69855640eb9c5191c
buqiyuan <buqiyuan@users.noreply.github.com>
2024-02-24 03:35:18 +0000

    Update ghPages 0256e00303d74be162ba0397808b5b94a60ebaa4

---
+++ playground/assets/wasm-VMJSURxR-xanw_U-y.js
@@ -0,0 +1,7 @@
+import{_ as n}from"./index-ZBp3hnf_.js";const r=async e=>{const a=await n(()=>import("./onig-CJPi_5NU-mBJmD8D5.js"),__vite__mapDeps([])).then(t=>t.default);return WebAssembly.instantiate(a,e).then(t=>t.instance.exports)};export{r as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
